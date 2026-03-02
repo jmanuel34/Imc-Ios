e5b46e9ccae8dd41412f85fb90d01a8d11fbfa0b
jm <jmanuel34@gmail.com>
2026-03-02 16:40:53 +0100

    Initial Commit

---
+++ Imc-Ios2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imc-Ios2
+//
+//  Created by Tardes on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
